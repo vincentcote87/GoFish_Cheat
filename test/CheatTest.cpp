3a098c781fd571114471f2a18b65a7cb94599c70
Vincent <vincent.cote@uleth.ca>
2018-03-11 22:09:30 -0600

    mocking up to 85%

--- test/CheatTest.cpp
+++ test/CheatTest.cpp
@@ -7,3 +7,117 @@
 #include "Card.h"
 #include "Game.h"
 #include "Cheat.h"
+
+TEST(CheatTest, dealCards) {
+	MockDeck deck;
+	MockUI ui;
+
+	Game* game = new Cheat(&ui, &deck);
+
+	game -> addPlayer(new Player("Vincent"));
+	game -> addPlayer(new Player("Krista"));
+
+	EXPECT_CALL(deck, getCard())
+	.Times(10)
+	.WillRepeatedly(Return (new Card(Card::HEART, Card::KING)));
+
+	EXPECT_CALL(deck, size())
+	.Times(2)
+	.WillRepeatedly(Return(10));
+
+	
+
+	game -> dealCards(game -> getPlayers());
+
+	for(Player* players : game -> getPlayers())
+		EXPECT_TRUE(players -> getHand() -> size() == 5);
+}
+
+TEST(CheatTest, runningGame) {
+	MockDeck deck;
+	MockUI ui;
+
+	Player* p1 = new Player("Vincent");
+	Player* p2 = new Player("Krista");
+
+	EXPECT_CALL(deck, shuffle())
+	.Times(1);
+
+	EXPECT_CALL(deck, size())
+	.Times(2)
+	.WillRepeatedly(Return(2));
+
+	EXPECT_CALL(deck, getCard())
+	.Times(2)
+	.WillOnce(Return(new Card(Card::HEART, Card::ACE)))
+	.WillOnce(Return(new Card(Card::HEART, Card::TWO)));
+	// .WillOnce(Return(new Card(Card::HEART, Card::ACE)))
+	// .WillOnce(Return(new Card(Card::HEART, Card::TWO)))
+	// .WillOnce(Return(new Card(Card::HEART, Card::TWO)))
+	// .WillOnce(Return(new Card(Card::HEART, Card::FOUR)))
+	// .WillOnce(Return(new Card(Card::HEART, Card::THREE)))
+	// .WillOnce(Return(new Card(Card::HEART, Card::SIX)))
+	// .WillOnce(Return(new Card(Card::HEART, Card::FIVE)))
+	// .WillOnce(Return(new Card(Card::HEART, Card::SIX)));
+
+	EXPECT_CALL(ui, playerName(_))
+	.Times(AtLeast(1));
+
+	EXPECT_CALL(ui, currentFace(_))
+	.Times(AtLeast(1));
+
+	EXPECT_CALL(ui, noCardsPlayed())
+	.Times(1);
+
+	EXPECT_CALL(ui, requestCard(_))
+	.Times(AtLeast(1))
+	.WillOnce(Return(54))
+	.WillOnce(Return(0))
+	.WillOnce(Return(54))
+	.WillOnce(Return(0))
+	.WillOnce(Return(54))
+	.WillRepeatedly(Return(54));
+
+	// .WillOnce(Return(1))
+	// .WillOnce(Return(1))
+	// .WillOnce(Return(54))
+	// .WillOnce(Return(1))
+	// .WillOnce(Return(1))
+	// .WillOnce(Return(54))
+	// .WillOnce(Return(1))
+	// .WillOnce(Return(54))
+	// .WillOnce(Return(1))
+	// .WillOnce(Return(1));
+	// .WillOnce(Return(1))
+	// .WillOnce(Return(54))
+	// .WillOnce(Return(1))
+	// .WillOnce(Return(54))
+	// .WillOnce(Return(1))
+	// .WillOnce(Return(54));
+
+
+	EXPECT_CALL(ui, callCheat(_))
+	.Times(AtLeast(1))
+	.WillOnce(Return(3))
+	.WillOnce(Return(3))
+	.WillOnce(Return(3))
+	.WillOnce(Return(1))
+	.WillOnce(Return(2));
+
+	// EXPECT_CALL(ui, playSucceeded())
+	// .Times(1);
+
+	// EXPECT_CALL(ui, playFailed())
+	// .Times(1);
+
+	EXPECT_CALL(ui, showScores(_))
+	.Times(1);
+
+
+	Game* game = new Cheat(&ui, &deck);
+
+	game -> addPlayer(p1);
+	game -> addPlayer(p2);
+
+	game -> start();
+}
